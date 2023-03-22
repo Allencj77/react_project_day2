8b23c26474afbb01f832f43d7390dae368d3e0bf
CHRIS ALLEN <727721euec015@skcet.ac.in>
2023-03-22 22:59:42 +0530

    msg

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React from 'react';
+function World() {
+  return(
+     <div>
+          <p><center>This is World Functional Component</center><hr></hr></p>
+     </div>
   );
 }
+class World1 extends React.Component
+{
+     render()
+     {
+         return <p><center>This is World Class Component</center><hr></hr></p>
+     }
+}
 
-export default App;
+export {World,World1};
+
+
+
---
+++ src/Components/Test1.jsx
@@ -0,0 +1,4 @@
+const My=()=>{
+    return <h1>Good Morning</h1>
+}
+export default My;
